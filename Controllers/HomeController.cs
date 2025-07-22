7ee08f0bfb3d69680e8fdaea1dab9c1da91c76a4
ubiracihotmail <ubiraci.domingos@hotmail.com>
2025-07-22 19:23:02 -0300

    [Ubiraci] Injetando o banco ao controller Home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Data;
 using Tarefas.Models;
 
 namespace Tarefas.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
